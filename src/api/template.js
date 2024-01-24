809cb9d282736a8d0d17544bfe50edd640be8112
熊熊熊子路 <jiaweima2000@163.com>
2024-01-24 16:13:31 +0800

    fix: 解决get请求不懈怠params数据的bug

--- src/api/template.js
+++ src/api/template.js
@@ -2,14 +2,27 @@ import request from '@/utils/request.js';
 import {GET, POST} from '@/config/requestMethods.js'
 
 /**
- * 模板配置，后期封装请求的时候按照现在这样子进行封装
+ * get请求模板配置，后期封装请求的时候按照现在这样子进行封装
  * @param data
  * @return {*}
  */
 export function testRequest(data) {
   return request({
-    url: '/api/test',
+    url: '/api/test/get',
     method: GET,
-    data
+    params: data
+  })
+}
+
+/**
+ * post请求模板配置，后期封装请求的时候按照现在这样子进行封装
+ * @param data
+ * @return {*}
+ */
+export function testPostRequest(data) {
+  return request({
+    url: '/api/test/post',
+    method: POST,
+    data: data
   })
 }
